696ebf6aebad7abfd8a351f90093ed5f4ef86b98
MOnagit config --global user.name <yurimelech@gmail.com>
2022-06-07 14:36:42 +0300

    adding func

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello");
+function addTax(price){
+      if(!price || price <= 0){
+         return 'Nothing is for free! please supply a price';
+         }
+      const vat = 1.17; 
+      const total = (price * vat).toFixed(2);
+      return total;
+      
+}
