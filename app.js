d37649bd725ce3c71fee7d58548c276ae67efb37
MOnagit config --global user.name <yurimelech@gmail.com>
2022-06-07 15:53:08 +0300

    end of lesson 7.6.22

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+
 function addTax(price){
-      if(!price || price <= 0){
-         return 'Nothing is for free! please supply a price';
+      if(!price || price <= 0 || typeof(price) === 'string' ){
+        throw Error ('Nothing is for free! please supply a price');
          }
       const vat = 1.17; 
       const total = (price * vat).toFixed(2);
-      return total;
-      
+      return total; 
 }
+ function generateId(){
+     // const rand = Math.random();
+     // console.log(`rand: ${rand}`);
+     // const num = rand * 100000;
+     // console.log(`num: ${num}`);
+     // const floor = Math.floor(num);
+     // console.log(`floor: ${floor}`);
+     // return floor;
+
+     const time = new Date().getTime();
+     return Math.floor(Math.random()*time);
+ }
+
+
+
