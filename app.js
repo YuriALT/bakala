e105bdb32141cbb72cd9dc0ec1b18f8cb180d499
MOnagit config --global user.name <yurimelech@gmail.com>
2022-06-11 18:43:25 +0300

    Updated 11.6

--- app.js
+++ app.js
@@ -1,71 +1,65 @@
+function addTax(price) {
+  if (!price || price <= 0 || typeof (price) === 'string') {
+      throw Error('Nothing is for free! please supply a price');
+  }
 
-function addTax(price){
-      if(!price || price <= 0 || typeof(price) === 'string' ){
-        throw Error ('Nothing is for free! please supply a price');
-         }
-      const vat = 1.17; 
-      const total = (price * vat).toFixed(2);
-      return total; 
+  const vat = 1.17; // = maam
+  const total = (price * vat).toFixed(2);
+  return total;
 }
 
-function generateId(){
-     const time = new Date().getTime();
-     return Math.floor(Math.random()*time);
- }
+function generateId() {
+  // const rand = Math.random();
+  // console.log(`rand: ${rand}`);
+  // const num = rand * 100000;
+  // console.log(`num: ${num}`);
+  // const floor = Math.floor(num);
+  // console.log(`floor: ${floor}`);
+  // return floor;
+
+  const time = new Date().getTime();
+  return Math.floor(Math.random() * time);
+}
 
 const products = [
-     {
-        id: generateId(),
-        price: 15,
-        name: 'Yellow cheese',
-        image:'2016/03/05/19/24/cheese-1238395_960_720.jpg',
-        description: 'Great product realy!',
-     },
-     {
-        id: generateId(),
-        price: 3,
-        name: 'Ice cream',
-        image:'2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
-        description: 'Be careful - very cold',
-     },
+  {
+      id: 1,
+      price: 15,
+      name: 'yellow cheese',
+      image: '2016/03/05/19/24/cheese-1238395_960_720.jpg',
+      description: 'great product really!',
+  },
+  {
+      id: 2,
+      price: 3,
+      name: 'ice cream',
+      image: '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
+      description: 'be careful - very cold',
+  },
 ];
 
-function filterProducts() {
-  const sortField = document.querySelector('#sort-field');
-  const sortDiraction = sort-field.value;
 
-  // sort array of products
-
-}
-
-function getProductsTemplate(){
-  const IMG_PATH ='https://cdn.pixabay.com/photo/'; //Configuration of the project
-  let html ='';
-
-  products.forEach(prod =>{
-    html += 
-    `<div class="card" id="card-{id}" style="width: 18rem;">
-      <img src="${IMG_PATH}${prod.image}"
-     " class="card-img-top" alt="${prod.description}">
-     <div class="card-body">
-       <h5 class="card-title">${prod.name}</h5>
-       <p class="card-text">$
-       ${addTax(prod.price)}</p>
-       <a href="#" class="btn btn-primary">View Detalis</a>
-    </div>
-  </div>`;
- })
- return html;
- }
-
-
+// take products data and add it into the html of each card
 function displayProduct() {
+  const IMG_PATH = 'https://cdn.pixabay.com/photo/'; // configuration of the project
   const container = document.getElementById('products-container');
-  const productsHtml = getProductsTemplate();
-  container.innerHTML = productsHtml;
+  let html = '';
+
+  products.forEach(prod => {
+      html +=
+          `<div class="card" style="width: 18rem;">
+              <img src="${IMG_PATH}${prod.image}"
+                  class="card-img-top" alt="${prod.description}">
+              <div class="card-body">
+                  <h5 class="card-title">${prod.name}</h5>
+                  <p class="card-text">
+                      ${addTax(prod.price)}</p>
+                  <a href="#" class="btn btn-primary">View Details</a>
+              </div>
+          </div>`;
+  })
+
+  container.innerHTML = html;
 }
 
 displayProduct();
-
-
-
