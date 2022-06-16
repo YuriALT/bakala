d47cc11ca8aa269d9773304301d808da0d49a6e1
MOnagit config --global user.name <yurimelech@gmail.com>
2022-06-16 12:04:38 +0300

    Added new func

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function addTax(price) {
   if (!price || price <= 0 || typeof (price) === 'string') {
-      throw Error('Nothing is for free! please supply a price');
+    throw Error('Nothing is for free! please supply a price');
   }
 
   const vat = 1.17; // = maam
@@ -21,45 +21,55 @@ function generateId() {
   return Math.floor(Math.random() * time);
 }
 
-const products = [
-  {
-      id: 1,
-      price: 15,
-      name: 'yellow cheese',
-      image: '2016/03/05/19/24/cheese-1238395_960_720.jpg',
-      description: 'great product really!',
+const products = [{
+    id: 1,
+    price: 15,
+    name: 'yellow cheese',
+    image: '2016/03/05/19/24/cheese-1238395_960_720.jpg',
+    description: 'great product really!',
   },
   {
-      id: 2,
-      price: 3,
-      name: 'ice cream',
-      image: '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
-      description: 'be careful - very cold',
+    id: 2,
+    price: 3,
+    name: 'ice cream',
+    image: '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
+    description: 'be careful - very cold',
   },
+  {
+    id: 3,
+    price: 25,
+    name: ' cream',
+    image: '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
+    description: 'old',
+  }
 ];
-
+function getTotalProducts(){
+  const total = document.getElementById('total-products');
+  total.innerHTML = products.length;
+}
 
 // take products data and add it into the html of each card
-function displayProduct() {
+function displayProducts() {
   const IMG_PATH = 'https://cdn.pixabay.com/photo/'; // configuration of the project
   const container = document.getElementById('products-container');
   let html = '';
 
   products.forEach(prod => {
-      html +=
-          `<div class="card" style="width: 18rem;">
+    html +=
+      `<div class="card" style="width: 18rem;">
               <img src="${IMG_PATH}${prod.image}"
                   class="card-img-top" alt="${prod.description}">
               <div class="card-body">
                   <h5 class="card-title">${prod.name}</h5>
                   <p class="card-text">
-                      ${addTax(prod.price)}</p>
+                      ${addTax(prod.price)}$</p>
                   <a href="#" class="btn btn-primary">View Details</a>
               </div>
           </div>`;
-  })
+  });
 
   container.innerHTML = html;
+  getTotalProducts();
 }
 
-displayProduct();
+displayProducts();
